e1b44dfcfdb1dc8a911589d3801ddde30d31356d
Syed Hassaan Ahmed <hassaan.brix@gmail.com>
2017-10-11 14:19:06 +0200

    Read kubectl proxy

--- kube-rest-api/Controllers/TenantsController.cs
+++ kube-rest-api/Controllers/TenantsController.cs
@@ -1,28 +1,47 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Kube.RestApi.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace Kube.RestApi
 {
     [Route("api/[controller]")]
     public class TenantsController : Controller
     {
+        private const string KubernetesEndpoint = "http://127.0.0.1:8001";
+        private const string MinecraftLabel = "minecraft";
+
+        private const int MinecraftPort = 25565;
+        private const int RconPort = 25575;
+
         // GET api/tenants
         [HttpGet]
-        public IEnumerable<Tenant> Get()
+        public async Task<IEnumerable<Tenant>> Get()
         {
-            return new Tenant[]
+            using (var client = new HttpClient())
             {
-                new Tenant
-                {
-                    Name = "tenant1",
-                    Endpoints = new Endpoints
+                client.BaseAddress = new Uri(KubernetesEndpoint);
+                var response = await client.GetAsync("/api/v1/services");
+
+                var stringResponse = await response.Content.ReadAsStringAsync();
+                var serviceList = JsonConvert.DeserializeObject<ServiceList>(stringResponse);
+
+                return serviceList.items
+                    .Where(i => i.spec.selector?.app == MinecraftLabel)
+                    .Select(i => new Tenant
                     {
-                        Minecraft = "10.10.10.10:25565",
-                        Rcon = "10.10.10.10:25575"
-                    }
-                }
-            };
+                        Name = i.metadata.name,
+                        Endpoints = new Endpoints
+                        {
+                            Minecraft = $"{i.status.loadBalancer.ingress.First().ip}:{MinecraftPort}",
+                            Rcon = $"{i.status.loadBalancer.ingress.First().ip}:{RconPort}"
+                        }
+                    });
+            }
         }
     }
 }
---
+++ kube-rest-api/Models/ServiceList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Kube.RestApi.Models
+{
+    public class ServiceList
+    {
+        public string kind { get; set; }
+        public string apiVersion { get; set; }
+        public Metadata metadata { get; set; }
+        public Item[] items { get; set; }
+    }
+
+    public class Metadata
+    {
+        public string selfLink { get; set; }
+        public string resourceVersion { get; set; }
+    }
+
+    public class Item
+    {
+        public Metadata1 metadata { get; set; }
+        public Spec spec { get; set; }
+        public Status status { get; set; }
+    }
+
+    public class Metadata1
+    {
+        public string name { get; set; }
+        public string _namespace { get; set; }
+        public string selfLink { get; set; }
+        public string uid { get; set; }
+        public string resourceVersion { get; set; }
+        public DateTime creationTimestamp { get; set; }
+        public Labels labels { get; set; }
+        public Annotations annotations { get; set; }
+    }
+
+    public class Labels
+    {
+        public string component { get; set; }
+        public string provider { get; set; }
+        public string app { get; set; }
+        public string chart { get; set; }
+        public string heritage { get; set; }
+        public string release { get; set; }
+        public string kubernetesioclusterservice { get; set; }
+        public string kubernetesioname { get; set; }
+        public string k8sapp { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Annotations
+    {
+        public string kubectlkubernetesiolastappliedconfiguration { get; set; }
+    }
+
+    public class Spec
+    {
+        public Port[] ports { get; set; }
+        public string clusterIP { get; set; }
+        public string type { get; set; }
+        public string sessionAffinity { get; set; }
+        public Selector selector { get; set; }
+    }
+
+    public class Selector
+    {
+        public string app { get; set; }
+        public string k8sapp { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Port
+    {
+        public string name { get; set; }
+        public string protocol { get; set; }
+        public int port { get; set; }
+        public object targetPort { get; set; }
+        public int nodePort { get; set; }
+    }
+
+    public class Status
+    {
+        public Loadbalancer loadBalancer { get; set; }
+    }
+
+    public class Loadbalancer
+    {
+        public Ingress[] ingress { get; set; }
+    }
+
+    public class Ingress
+    {
+        public string ip { get; set; }
+    }
+}
